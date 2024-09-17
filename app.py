01cda30a165438117b08c52cdc0649c0eb0cda69
klimeckj <61334136+klimeckj@users.noreply.github.com>
2024-09-17 08:23:26 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 import streamlit as st
+from google.cloud import firestore
+import json
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+# Authenticate to Firestore with the JSON account key.
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="ga4cdp")
 
+#  tady to zkusim vložit
+# Streamlit widgets to let a user create a new post
+user_email = st.text_input("User e-mail:")
+search = st.button("Search")
+
+# Once the user has submitted, upload it to the database
+if user_email and search:
+    doc_ref = db.collection("hephaestus_test").document(user_email)
+    doc = doc_ref.get()
+    st.write("searched e-mail: ", doc.id)
+    st.write("known informations: ", doc.to_dict())
+
+# Create a reference to the Google post.
+# doc_ref = db.collection("hephaestus_test").document(user_email)
+
+# Then get the data at that reference.
+#doc = doc_ref.get()
+
+# Let's see what we got!
+#st.write("searched e-mail: ", doc.id)
+#st.write("known informations: ", doc.to_dict())
 
